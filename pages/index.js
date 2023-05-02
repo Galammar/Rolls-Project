4803b6317e71018162b7dac0dee39405ba83ae18
Galammar <80973810+Galammar@users.noreply.github.com>
2023-05-02 18:00:21 +0300

    added players, needs Fix

--- pages/index.js
+++ pages/index.js
@@ -3,34 +3,75 @@ import Head from 'next/head';
 import { useState } from 'react';
 
 export default function Home() {
-  const [attackRolls, setAttackRolls] = useState('');
-  const [spells, setSpells] = useState('');
+  const [attackRoll, setAttackRoll] = useState('');
+  const [spell, setSpell] = useState('');
+  const adventurers = [new Player('Ronandt')];
   return (
     <div>
       <Head>
         <title>Project</title>
       </Head>
       <main>
+        <Panel name={adventurers[0].name}></Panel>
         <div className={styles.container}>
-          <div className={styles.attackDisplayer}>{attackRolls}</div>
+          <div className={styles.attackDisplayer}>{attackRoll}</div>
           <div className={styles.spellDisplayer}>Spell Something</div>
         </div>
         <div className={styles.container}>
           <button
             className={styles.attackButton}
-            onClick={() => setAttackRolls(Math.floor(Math.random() * 20))}
+            onClick={() => {
+              setAttackRoll(Math.floor(Math.random() * 21));
+              console.log(attackRoll);
+            }}
           >
             Attack!
             {/* Bring The Image In */}
           </button>
           <button
             className={styles.spellButton}
-            onClick={() => setSpells('Fireball')}
+            onClick={() => setSpell('Fireball')}
           >
             Spell!
           </button>
         </div>
       </main>
+    </div>
+  );
+}
+
+class Player {
+  name;
+  str;
+  dex;
+  con;
+  int;
+  wis;
+  cha;
+
+  constructor(
+    name,
+    str = 10,
+    dex = 10,
+    con = 10,
+    int = 10,
+    wis = 10,
+    cha = 10
+  ) {
+    this.name = name;
+    this.str = str;
+    this.dex = dex;
+    this.con = con;
+    this.int = int;
+    this.wis = wis;
+    this.cha = cha;
+  }
+}
+
+function Panel(name) {
+  return (
+    <div>
+      <div>{name}</div>
     </div>
   );
 }
